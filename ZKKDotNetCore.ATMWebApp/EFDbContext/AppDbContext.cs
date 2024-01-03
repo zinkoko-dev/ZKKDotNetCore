ff97628d8ddacdc4797d05d2b22f2e56cb440225
Zin Ko Ko <zinkoko.dev@gmail.com>
2024-01-03 14:46:19 +0630

    [Complete] add dbcontext

---
+++ ZKKDotNetCore.ATMWebApp/EFDbContext/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZKKDotNetCore.ATMWebApp.Models;
+
+namespace ZKKDotNetCore.ATMWebApp.EFDbContext;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<CardHolderDataModel> CardHolders { get; set; }
+
+    public CardHolderDataModel GetCardHolderByCardNumAndPin(string cardNum, int pin)
+    {
+        return CardHolders.FirstOrDefault(user => 
+            user.CardNumber == cardNum && user.Pin == pin
+        );
+    }
+}
