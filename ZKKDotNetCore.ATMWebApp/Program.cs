1162eb7ae73fd7431cda51911b81abbc5c037d84
Zin Ko Ko <zinkoko.dev@gmail.com>
2024-01-03 15:44:46 +0630

    [Complete] deposit, withdraw, show balance and exit

--- ZKKDotNetCore.ATMWebApp/Program.cs
+++ ZKKDotNetCore.ATMWebApp/Program.cs
@@ -1,7 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using ZKKDotNetCore.ATMWebApp.EFDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    string? connectionString = builder.Configuration.GetConnectionString("DbConnection");
+    opt.UseSqlServer(connectionString);
+});
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(5);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -17,11 +35,12 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=AtmAction}/{action=Index}/{id?}");
 
 app.Run();
+
