1b8ae11a32c44aab694a9feb19f6930606304320
Zin Ko Ko <zinkoko.dev@gmail.com>
2024-01-03 14:48:44 +0630

    [Complete] add models

---
+++ ZKKDotNetCore.ATMWebApp/Models/CardHolderDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZKKDotNetCore.ATMWebApp.Models;
+
+[Table("Tbl_CardHolder")]
+public class CardHolderDataModel
+{
+    [Key]
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string CardNumber { get; set; }
+    public int Pin { get; set; }
+    public Decimal Balance { get; set; }
+}
---
+++ ZKKDotNetCore.ATMWebApp/Models/CardHolderResponseModel.cs
@@ -0,0 +1,24 @@
+﻿namespace ZKKDotNetCore.ATMWebApp.Models;
+
+public class CardHolderResponseModel
+{
+    public string CardNumber { get; set; }
+    public int Pin { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Decimal Balance { get; set; }
+    public Decimal DepositAmt { get; set; }
+    public Decimal WithdrawAmt { get; set; }
+}
+
+public class MessageModel
+{
+    public MessageModel() { }
+    public MessageModel(bool isSuccess, string message)
+    {
+        IsSuccess = isSuccess;
+        Message = message;
+    }
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+}
