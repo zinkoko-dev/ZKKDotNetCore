6e30bb3ddc3bedc2fec3a15de5d4f97eca58a481
Zin Ko Ko <zinkoko.dev@gmail.com>
2024-01-09 23:29:58 +0630

    [Complete] HttpClient (MVC) Read

---
+++ ZKKDotNetCore.MvcApp/Controllers/StudentHttpClientController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ZKKDotNetCore.MvcApp.Models;
+
+namespace ZKKDotNetCore.MvcApp.Controllers
+{
+    public class StudentHttpClientController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public StudentHttpClientController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            StudentListResponseModel model = new StudentListResponseModel();
+            var response = await _httpClient.GetAsync("/api/Student");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                model = JsonConvert.DeserializeObject<StudentListResponseModel>(jsonStr)!;
+            }
+            return View("~/Views/StudentRefit/Index.cshtml", model);
+        }
+    }
+}
