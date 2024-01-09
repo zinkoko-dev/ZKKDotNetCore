f41064bc17cee9941491cc03745eea3bdb12d10e
Zin Ko Ko <zinkoko.dev@gmail.com>
2024-01-09 23:30:22 +0630

    [Complete] Refit (MVC) Read

---
+++ ZKKDotNetCore.MvcApp/Controllers/StudentRefitController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZKKDotNetCore.MvcApp.Interfaces;
+
+namespace ZKKDotNetCore.MvcApp.Controllers
+{
+    public class StudentRefitController : Controller
+    {
+        private readonly IStudentApi _iStudentApi;
+
+        public StudentRefitController(IStudentApi iStudentApi)
+        {
+            _iStudentApi = iStudentApi;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _iStudentApi.GetStudents();
+            return View(model);
+        }
+    }
+}
