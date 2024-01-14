9969ce112b45e1eb253bb0d8f01024c57be63989
Zin Ko Ko <zinkoko.dev@gmail.com>
2024-01-14 21:43:17 +0630

    [Complete] Add Models and AppDbContext

---
+++ ZKKDotNetCore.MinimalApi/Models/StudentDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ZKKDotNetCore.MinimalApi.Models
+{
+    [Table("Tbl_Student")]
+    public class StudentDataModel
+    {
+        [Key]
+        public int Student_Id { get; set; }
+        public string Student_Name { get; set; }
+        public string Student_City { get; set; }
+        public string Student_Gender { get; set; }
+
+    }
+}
---
+++ ZKKDotNetCore.MinimalApi/Models/StudentResponseModle.cs
@@ -0,0 +1,16 @@
+﻿namespace ZKKDotNetCore.MinimalApi.Models
+{
+    public class StudentResponseModle
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public StudentDataModel StudentDataModel { get; set; }
+    }
+
+    public class StudentListResponseModel
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public List<StudentDataModel> ListStudentDataModel { get; set; }
+    }
+}
