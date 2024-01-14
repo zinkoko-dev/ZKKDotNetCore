8fb668756c9e76e0476f35cb02a3433eb71fe1d4
Zin Ko Ko <zinkoko.dev@gmail.com>
2024-01-14 22:29:09 +0630

    [Complete] GetStudent(Pagination)

---
+++ ZKKDotNetCore.MinimalApi/Features/Student/StudentService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZKKDotNetCore.MinimalApi.Features.Student
+{
+    public static class StudentService
+    {
+        public static void AddStudentService(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/Student/{pageNo}/{pageSize}", async ([FromServices] AppDbContext db, int pageNo, int pageSize) =>
+            {
+                return await db.Students
+                .AsNoTracking()
+                .Skip((pageNo - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            })
+            .WithName("GetStudents")
+            .WithOpenApi();
+        }
+    }
+}
