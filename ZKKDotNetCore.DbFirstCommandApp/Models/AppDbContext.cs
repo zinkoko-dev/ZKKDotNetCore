def7f981cfe23d2c6aed03405141ffdad8ebd5a3
Zin Ko Ko <zinkoko.dev@gmail.com>
2024-01-21 23:34:49 +0630

    [Complete] Db First Command Application

---
+++ ZKKDotNetCore.DbFirstCommandApp/Models/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZKKDotNetCore.DbFirstCommandApp.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblStudent> TblStudents { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=.;Database=ZKKDotNetCore;User ID=sa;Password=sasa;Trusted_Connection=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblStudent>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK_Tbl_Student_1");
+
+            entity.ToTable("Tbl_Student");
+
+            entity.Property(e => e.StudentId).HasColumnName("Student_Id");
+            entity.Property(e => e.StudentCity)
+                .HasMaxLength(50)
+                .HasColumnName("Student_City");
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(50)
+                .HasColumnName("Student_Gender");
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(50)
+                .HasColumnName("Student_Name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ZKKDotNetCore.DbFirstCommandApp/Models/TblStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZKKDotNetCore.DbFirstCommandApp.Models;
+
+public partial class TblStudent
+{
+    public int StudentId { get; set; }
+
+    public string StudentName { get; set; } = null!;
+
+    public string StudentCity { get; set; } = null!;
+
+    public string StudentGender { get; set; } = null!;
+}
